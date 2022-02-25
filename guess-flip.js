88f57a7221c22d22dff2a7ec1ec55248746728ae
Gaurav Rao <69742533+Gauravmrao@users.noreply.github.com>
2022-02-25 00:52:53 -0500

    Update guess-flip.js

--- guess-flip.js
+++ guess-flip.js
@@ -1,20 +1,24 @@
 import {coinFlip, coinFlips, countFlips, flipACoin} from '/home/runner/work/a02-Gauravmrao/a02-Gauravmrao/modules/coin.mjs';
 
 let inputData = process.argv;
-if (inputData.length < 3) {
+
+let guessInput = inputData.slice(2);
+
+let guessCoin = guessInput[0];
+
+if (guessCoin != "heads" && guessCoin != "tails") {
     console.log("Error: no input.");
     console.log("Usage: node guess-flip --call=[heads|tails])");
 } else {
-    let guessInput = inputData.slice(2);
-
-    let guessCoin = guessInput[0];
-    
-    if (guessCoin != "heads" && guessCoin != "tails") {
-        console.log("Error: no input.");
-        console.log("Usage: node guess-flip --call=[heads|tails])");
-    } else {
-        console.log(flipACoin(guessCoin));
-    }
+    console.log(flipACoin(guessCoin));
 }
 
 
+
+
+/*
+if (inputData.length < 3) {
+    console.log("Error: no input.");
+    console.log("Usage: node guess-flip --call=[heads|tails])");
+}
+*/
