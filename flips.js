da3c19e73c08debe1d209fa4179c054145047fc4
Gaurav Rao <69742533+Gauravmrao@users.noreply.github.com>
2022-02-24 23:57:35 -0500

    fixed import statements for autograder

--- flips.js
+++ flips.js
@@ -1,7 +1,7 @@
 // Import statement
 
-// import {coinFlip, coinFlips, countFlips, flipACoin} from '/home/runner/work/a02-Gauravmrao/a02-Gauravmrao/modules/coin.mjs';
-import {coinFlip, coinFlips, countFlips, flipACoin} from '/Users/grao2/Documents/UNC Chapel Hill/Sophomore Year Classwork/Spring Semester/comp426/a02-Gauravmrao/modules/coin.mjs';
+import {coinFlip, coinFlips, countFlips, flipACoin} from '/home/runner/work/a02-Gauravmrao/a02-Gauravmrao/modules/coin.mjs';
+// import {coinFlip, coinFlips, countFlips, flipACoin} from '/Users/grao2/Documents/UNC Chapel Hill/Sophomore Year Classwork/Spring Semester/comp426/a02-Gauravmrao/modules/coin.mjs';
 
 
 let flipsInput = process.argv.slice(2);
