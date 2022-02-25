4a73661bb0e6f051777c060bdaa4038baaf34654
Gaurav Rao <69742533+Gauravmrao@users.noreply.github.com>
2022-02-24 22:48:05 -0500

    testing the autograder

--- flips.js
+++ flips.js
@@ -1,5 +1,7 @@
 // Import statement
-import {coinFlip, coinFlips, countFlips, flipACoin} from '/Users/grao2/Documents/UNC Chapel Hill/Sophomore Year Classwork/Spring Semester/comp426/a02-Gauravmrao/modules/coin.mjs';
+
+import {coinFlip, coinFlips, countFlips, flipACoin} from '/home/runner/work/a02-Gauravmrao/a02-Gauravmrao/modules/coin.mjs';
+// import {coinFlip, coinFlips, countFlips, flipACoin} from '/Users/grao2/Documents/UNC Chapel Hill/Sophomore Year Classwork/Spring Semester/comp426/a02-Gauravmrao/modules/coin.mjs';
 
 
 let inputArray = process.argv;
